c4cf411cd26f098ffe833cf1334fe50f6035e72a
rainy58 <119713626+yhg4908@users.noreply.github.com>
2024-12-29 10:13:45 +0900

    Add files via upload

---
+++ discordlite/intents.py
@@ -0,0 +1,39 @@
+# discordlite/intents.py
+
+class Intents:
+    """Handles bot intents similar to discord.py"""
+    GUILDS = 1 << 0
+    GUILD_MEMBERS = 1 << 1
+    GUILD_BANS = 1 << 2
+    GUILD_EMOJIS_AND_STICKERS = 1 << 3
+    GUILD_INTEGRATIONS = 1 << 4
+    GUILD_WEBHOOKS = 1 << 5
+    GUILD_INVITES = 1 << 6
+    GUILD_VOICE_STATES = 1 << 7
+    GUILD_PRESENCES = 1 << 8
+    GUILD_MESSAGES = 1 << 9
+    GUILD_MESSAGE_REACTIONS = 1 << 10
+    GUILD_MESSAGE_TYPING = 1 << 11
+    DIRECT_MESSAGES = 1 << 12
+    DIRECT_MESSAGE_REACTIONS = 1 << 13
+    DIRECT_MESSAGE_TYPING = 1 << 14
+    MESSAGE_CONTENT = 1 << 15
+    GUILD_SCHEDULED_EVENTS = 1 << 16
+
+    def __init__(self):
+        self.flags = 0
+
+    @staticmethod
+    def default():
+        """Returns default intents"""
+        intents = Intents()
+        intents.flags = Intents.GUILDS | Intents.GUILD_MESSAGES
+        return intents
+
+    def enable(self, intent):
+        """Enables a specific intent"""
+        self.flags |= intent
+
+    def disable(self, intent):
+        """Disables a specific intent"""
+        self.flags &= ~intent
